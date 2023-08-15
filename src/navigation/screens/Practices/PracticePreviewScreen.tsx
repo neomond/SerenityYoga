a61be66b758f7d140d534694578174176f54c434
neomond <neoweise@yahoo.com>
2023-08-15 15:33:30 +0400

    added UI for videos preview

--- src/navigation/screens/Practices/PracticePreviewScreen.tsx
+++ src/navigation/screens/Practices/PracticePreviewScreen.tsx
@@ -1,4 +1,5 @@
 import {
+  Image,
   ImageBackground,
   StyleSheet,
   Text,
@@ -26,12 +27,78 @@ const PracticePreviewScreen = ({navigation}: any) => {
       </ImageBackground>
       <BottomSheetComponent
         isVisible={true}
-        snapPoints={['25%', '20%', '80%']}
+        snapPoints={['25%', '27%', '80%']}
         toggleBottomSheet={() => {
           console.log('meow');
         }}>
         <View style={styles.bottomSheetContent}>
-          <Text>Bottom Sheet Content</Text>
+          <View style={styles.bottomshtopCont}>
+            <View>
+              <Text style={styles.bottomshHeaderText}>Morning Yoga Flow</Text>
+              <Text>with Elly</Text>
+            </View>
+            <Image
+              style={{width: 50, height: 50, borderRadius: 25}}
+              source={require('../../../assets/test.png')}
+            />
+          </View>
+          <TouchableOpacity style={styles.startBtn}>
+            <Text style={styles.startBtnText}>Start Workout</Text>
+          </TouchableOpacity>
+        </View>
+        <View
+          style={{
+            backgroundColor: '#f5f5f5',
+            width: '100%',
+            height: '100%',
+          }}>
+          <View style={styles.descFirst}>
+            <View>
+              <Text>10 min</Text>
+            </View>
+            <View>
+              <Text>Low intensity</Text>
+            </View>
+          </View>
+
+          <Text style={{marginHorizontal: 20, fontSize: 16}}>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iusto
+            sequi nisi illum quisquam doloribus quasi ut, eos deleniti
+            recusandae natus magnam, corporis quaerat commodi numquam
+            blanditiis, nobis a impedit dolor. Lorem ipsum dolor sit amet
+            consectetur adipisicing elit.Namaste
+          </Text>
+          <View style={{marginHorizontal: 20, marginVertical: 20}}>
+            <Text style={styles.bottomshHeaderText}>Workout Structure</Text>
+            <View
+              style={{
+                marginTop: 12,
+              }}>
+              <View
+                style={{
+                  backgroundColor: '#fff',
+                  borderRadius: 12,
+                  padding: 18,
+                  flexDirection: 'row',
+                  justifyContent: 'space-between',
+                  marginBottom: 8,
+                }}>
+                <Text>🧘‍♀️ Workout</Text>
+                <Text>10 min</Text>
+              </View>
+              <View
+                style={{
+                  backgroundColor: '#fff',
+                  borderRadius: 12,
+                  padding: 18,
+                  flexDirection: 'row',
+                  justifyContent: 'space-between',
+                }}>
+                <Text>😌 Shavasana meditation</Text>
+                <Text>5 min</Text>
+              </View>
+            </View>
+          </View>
         </View>
       </BottomSheetComponent>
     </GestureHandlerRootView>
@@ -70,5 +137,34 @@ const styles = StyleSheet.create({
     top: 60,
     position: 'absolute',
     width: '100%',
+  },
+  bottomshHeaderText: {fontSize: 20, fontWeight: '600', paddingBottom: 5},
+  bottomshtopCont: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  startBtn: {
+    width: '100%',
+    marginTop: 30,
+    backgroundColor: '#815cff',
+    paddingVertical: 15,
+    borderRadius: 30,
+  },
+  startBtnText: {
+    textAlign: 'center',
+    color: '#fff',
+    fontWeight: '500',
+    fontSize: 15,
+  },
+  descFirst: {
+    marginVertical: 20,
+    marginHorizontal: 20,
+    backgroundColor: '#fff',
+    padding: 25,
+    flexDirection: 'row',
+    columnGap: 20,
+    // justifyContent: 'space-between',
+    borderRadius: 20,
   },
 });
